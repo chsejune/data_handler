3608c7b8916dea358838dc58da4eb2385921e1d6
sjcLab <chsejune@naver.com>
2017-08-01 14:12:13 +0900

    000001.jpg

--- pandas_basics.py
+++ pandas_basics.py
@@ -19,6 +19,10 @@ sub_df = df.filter(regex=r'I[0-9]') # regular expression 이용, # 열 목록 �
 sub_df = df['Y'] # 'Y' 라는 이름을 가진 열 데이터 추출
 
 
+# 명시된 컬럼 지우기
+sub_df.drop(['A', 'B'], axis=1, inplace=True) # 'A' 컬럼과 'B' 컬럼 지우기, axis=1 지정을 통해 해당 컬럼 지우기, axis=0 일 경우 해당 row 가 지워짐, inplace=True 지정을 통해 sub_df=sub_df.drop(['A', 'B'], axis=1) 이렇게 안해도 됨(re-assign option).
+
+
 # 각각의 데이터 조건이 맞는 경우 추출하기
 sub_df = df[df['Y']==0] # df['Y'] 열의 값들 중 값이 '0' 인 경우의 df 데이터들 추출하기 (df와 sub_df의 열 개수는 같음)
 
